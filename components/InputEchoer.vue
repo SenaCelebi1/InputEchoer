8589a63b53f68e20a7ca41169c5831440f5e7a83
Sena Celebi <43573954+SenaCelebi@users.noreply.github.com>
2021-10-28 15:22:53 +0300

    First Commit

---
+++ components/InputEchoer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div >
+    <h1>Input Echoer</h1>
+     <input v-model="message" placeholder="edit me">
+     <p>Message is: {{ message }}</p>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    message: String
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <InputEchoer/>
+</template>
+
+<script>
+export default {}
+</script>
